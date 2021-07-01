196668af9ad876d2a55e3cb4a712df96f002a797
RebeccaFritz <86808098+RebeccaFritz@users.noreply.github.com>
2021-07-01 17:53:23 -0500

    Create script.js
    This contains the javascript for the entire website

---
+++ script.js
@@ -0,0 +1,28 @@
+// When the use clicks on the dots, open the popup
+function popUpFunction1() {
+  var popup1 = document.getElementById("myPopup1");
+  popup1.classList.toggle("show1");
+}
+
+function popUpFunction2() {
+  var popup2 = document.getElementById("myPopup2");
+  popup2.classList.toggle("show2");
+}
+
+function popUpFunction3() {
+  var popup3 = document.getElementById("myPopup3");
+  popup3.classList.toggle("show3");
+}
+
+function popUpFunction4() {
+  var popup4 = document.getElementById("myPopup4");
+  popup4.classList.toggle("show4");
+}
+
+function popUpFunction5() {
+  var popup5 = document.getElementById("myPopup5");
+  popup5.classList.toggle("show5");
+}
+
+// Close the popups when a new one is opened
+
